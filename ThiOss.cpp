1664ebda3ebf3f1df68cb2861b3ec0bbcd55f266
bangbuistu <76458781+bangbuistu@users.noreply.github.com>
2022-11-11 02:44:35 +0700

    May09

--- ThiOss.cpp
+++ ThiOss.cpp
@@ -1,12 +1,23 @@
 
 #include <iostream>
 using namespace std;
-
+//Ham giai phuong trinh bac 1
+bool pt1(float a, float b, float &nghiem)
+{
+	if (a == 0)
+		return false;
+	nghiem = -b / a;
+	return true;
+}
 int main()
 {
 	/*
-	SV lay nhanh co ten chinh la so may minh ngoi. De thi co trong nhanh do.
-	Vi du ban ngoi may 1 thi vao nhanh m01 de lay de thi.
+	De thi:
+	1/ Ham pt1 tren co loi khong?
+	Neu co thi hay tao ra 1 ban sao cua nhanh ban dang lam co ten la m??_moi de fix loi
+	2/ Neu ham pt1 khong co loi thi hay tao ra nhanh moi co ten la m??_moi de xay dung them chuc nang giai phuong trinh bac 2
+	3/ Viet ham main de goi ham cac tren 
+	4/ Tron nhanh m??_moi vao nhanh m??
 	*/
 	return 0;
 }
