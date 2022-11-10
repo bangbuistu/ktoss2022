79324dea83abe1e4c096e6456459e33f71c6f855
Bang <bang.buinhat@stu.edu.vn>
2022-11-11 02:26:34 +0700

    main

---
+++ ThiOss.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	SV lay nhanh co ten chinh la so may minh ngoi. De thi co trong nhanh do.
+	Vi du ban ngoi may 1 thi vao nhanh m01 de lay de thi.
+	*/
+	return 0;
+}
