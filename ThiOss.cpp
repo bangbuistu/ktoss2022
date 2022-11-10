394c03d9312209f83bcbda59e5ea211203c4b2b3
bangbuistu <76458781+bangbuistu@users.noreply.github.com>
2022-11-11 02:46:43 +0700

    May12

--- ThiOss.cpp
+++ ThiOss.cpp
@@ -1,12 +1,26 @@
 
 #include <iostream>
 using namespace std;
-
+//Ham tinh chu vi hinh chu nhat
+float chuViHinhChuNhat(float dai, float rong) {
+	if (dai <= 0 || rong <= 0)
+	{
+		cout << "Canh hinh chu nhat phai lon hon 0";
+		return 0;
+	}
+	return (dai+rong)*2;
+}
 int main()
 {
 	/*
-	SV lay nhanh co ten chinh la so may minh ngoi. De thi co trong nhanh do.
-	Vi du ban ngoi may 1 thi vao nhanh m01 de lay de thi.
+	De thi:
+	1/ Ham chuViHinhChuNhat tren co loi khong?
+	Neu co thi hay tao ra 1 ban sao cua nhanh ban dang lam co ten la m??_moi de fix loi
+	2/ Neu ham chuViTamGiac tren co loi thi hay tao ra nhanh moi co ten la m??_moi de xay dung them chuc nang tinh dien tich hinh chu nhat
+	3/ Viet ham main de goi ham tren																																																																																																																																																																																								
+	4/ Tron nhanh m??_moi vao nhanh m??
 	*/
+	
+	system("pause");
 	return 0;
 }
