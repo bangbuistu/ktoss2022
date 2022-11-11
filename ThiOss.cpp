9fb2f9136d6a8ea826880bb0f39ca29143221826
thi <thi@.com>
2022-11-11 13:57:37 +0700

    casu1

--- ThiOss.cpp
+++ ThiOss.cpp
@@ -15,7 +15,17 @@ int main()
 	/*
 	De thi:
 	1/ Ham chuViTamGiac tren co loi khong?
+	 có lỗi
 	Neu co thi hay tao ra 1 ban sao cua nhanh ban dang lam co ten la m??_moi de fix loi
+	
+	float chuViTamGiac(float a, float b, float c) {
+	if (a >= 0 || b >= 0 || c >= 0)
+	{
+		cout << "Canh tam giac phai lon hon 0";
+		return 0;
+	}
+	return a + b + c;
+}
 	2/ Neu ham chuViTamGiac tren co loi thi hay tao ra nhanh moi co ten la m??_moi de xay dung them chuc nang tinh dien tich tam giac
 	3/ Tron nhanh m??_moi vao nhanh m??
 	*/
