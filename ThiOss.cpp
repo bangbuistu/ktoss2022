80b84cfa1078ca6e7220028a2542f542d6e22716
taicq <taicq111@gmail.com>
2022-12-17 09:33:10 +0700

    them ham giai pt bac 2

--- ThiOss.cpp
+++ ThiOss.cpp
@@ -35,7 +35,7 @@ int main()
 	1/ Ham pt1 tren co loi khong?
 	Neu co thi hay tao ra 1 ban sao cua nhanh ban dang lam co ten la m??_moi de fix loi
 	=>Ham pt1 tren khong co loi
-	
+
 	2/ Neu ham pt1 khong co loi thi hay tao ra nhanh moi co ten la m??_moi de xay dung them chuc nang giai phuong trinh bac 2
 	3/ Viet ham main de goi ham cac tren 
 	4/ Tron nhanh m??_moi vao nhanh m??
