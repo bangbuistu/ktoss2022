0d913aabb3ee135233bd1b24c7aff3774b700df9
Ninh <you@example.comdh51905239@gmail.com>
2022-12-17 09:44:17 +0700

    da tra loi cau hoi

--- ThiOss.cpp
+++ ThiOss.cpp
@@ -1,41 +1,28 @@
 
 #include <iostream>
 using namespace std;
-//Ham tinh chu vi hinh vuong
-float chuViHinhVuong(float canh) {
-	if (canh <= 0)
-	{
-		cout << "Canh hinh vuong phai lon hon 0";
-		return 0;
-	}
-	return canh * 4;
-}
-float dienTichHinhVuong(float canh){
-	if (canh <=0 )
-	{
-		cout<<"Canh hinh vuong phai lon hon 0";
-		return 0;
-	}
-	return canh*canh;
+// Vu thi ninh
+//Ham cho biet la tam giac gi
+void tamGiac(float a, float b, float c)
+{
+	if (a == b && b == c)
+		cout << "tam giac deu";
+	else if (a == b || b == c || c == a)
+		cout << "tam giac can";
+	else if ((c*c == (a*a + b*b)) || (b*b == (a*a + c*c)) || (a*a == (c*c + b*b)))
+		cout << "Tam giac vuong";
+	else
+		cout << "tam giac thuong";
 }
 int main()
 {
 	/*
 	De thi:
-	1/ Ham chuViHinhVuong tren co loi khong?
-	TL: Ham chu vi hinh vuong khong co loi
+	1/ Ham tamGiac tren co loi khong?
 	Neu co thi hay tao ra 1 ban sao cua nhanh ban dang lam co ten la m??_moi de fix loi
-	2/ Neu ham chuViHinhVuong tren co loi thi hay tao ra nhanh moi co ten la m??_moi de xay dung them chuc nang tinh dien tich hinh vuong
-	TL: Ham khong chu vi co loi
-		Bonus luon ham tinh dien tich =]]
-	3/ Viet ham main de goi ham tren
+	2/ Neu ham tamGiac khong co loi thi hay tao ra nhanh moi co ten la m??_moi de xay dung them chuc nang tinh dien tich tam giac
+	3/ Viet ham main de goi ham cac tren
 	4/ Tron nhanh m??_moi vao nhanh m??
 	*/
-	float a;
-	cout<<"Nhap canh hinh vuong:"<<endl;
-	cin>> a;
-	cout<<"Chu vi hinh vuonng :"<<chuViHinhVuong(a)<<endl;
-	cout<<"Dien tich hinh vuonng :"<<dienTichHinhVuong(a)<<endl;
-	system("pause");
 	return 0;
 }
