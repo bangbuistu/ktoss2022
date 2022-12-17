bee15c62351b62afdc176e11864c1b429a6ff260
luan <you@example.com>
2022-12-17 09:18:16 +0700

    addnew

--- ThiOss.cpp
+++ ThiOss.cpp
@@ -12,15 +12,28 @@ float dienTichTamGiac(float a, float b, float c) {
 
 	return sqrt(p*(p-a)*(p-b)*(p-c));
 }
+float chuvi(int a, int b, int c) {
+ return a + b + c;
+}
 int main()
 {
 	/*
 	De thi:
 	1/ Ham dienTichTamGiac tren co loi khong?
+	//TRẢ LỜI: hàm trên không có lỗi.
+	
 	Neu co thi hay tao ra 1 ban sao cua nhanh ban dang lam co ten la m??_moi de fix loi
+	
 	2/ Neu ham dienTichTamGiac khong co loi thi hay tao ra nhanh moi co ten la m??_moi de xay dung them chuc nang tinh chu vi tam giac
+	// TRẢ LỜI: Em tạo một nhánh tên luan và thêm chức năng chu vi tam giác.
+	
 	3/ Tron nhanh m??_moi vao nhanh m??
 	*/
+	
+	float a,b,c;
+	cout<<" nhap canh: ";
+	cin>>a,b,c;
+	cout>>"\nChu vi : " chuvi(a,b,c)<<"\nDien tich: "<<dienTichTamGiac(a,b,c);
 	system("pause");
 	return 0;
 }
